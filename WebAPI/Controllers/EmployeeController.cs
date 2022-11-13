3263b470717e40b018189de731deb3b789f3581d
Nader Saoudy <developernader@gmail.com>
2022-11-13 03:30:43 +0300

    Added HttpResponseMessage method Get Employees

--- WebAPI/Controllers/EmployeeController.cs
+++ WebAPI/Controllers/EmployeeController.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -9,5 +12,23 @@ namespace WebAPI.Controllers
 {
     public class EmployeeController : ApiController
     {
+        public HttpResponseMessage Get()
+        {
+            DataTable table = new DataTable();
+            string query = @"select * from Employees";
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["conString"].ConnectionString))
+            {
+                using (var cmd = new SqlCommand(query, con))
+                {
+                    using (var da = new SqlDataAdapter(cmd))
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        da.Fill(table);
+                    }
+                }
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
     }
 }
