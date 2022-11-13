689545414667ed93f11dc808c7cd4f3e221a2a4c
Nader Saoudy <developernader@gmail.com>
2022-11-13 03:05:38 +0300

    Add project files.

---
+++ WebAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using WebAPI.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WebAPI.Controllers
+{
+    public class DepartmentController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            DataTable dt = new DataTable();
+            string query = @"select DepartmentID,DepartmentName 
+                             from Departments";
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["conString"].ConnectionString))
+            {
+
+            }
+
+
+        }
+    }
+}
